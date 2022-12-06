54bf146a012ea6d82801188aaf1f05592fc1cf07
AWESOME319 <83673925+AWESOME319@users.noreply.github.com>
2022-12-06 15:55:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-song1="";
-song2="";
+Song1="";
+Song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+scoreLeftWrist=0;
+scoreRightWrist=0;
+songplaying="";
 function preload()
 {
-    song1=loadSound("song1");
-    song2=loadSound("song2");
+    Song1=loadSound("song1");
+    Song2=loadSound("song2");
 }
 function setup()
 {
@@ -11,8 +18,40 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
         video.hide();
+        poseNet=ml5.poseNet(video,modelLoaded);
+        poseNet.on('pose',gotPoses);
 }
 function draw()
 {
-    image( video,0,0,600,500)
+    image( video,0,0,600,500);
+    song_variable.isPlaying()
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreLeftWrist>0.2)
+    {
+        circle(rightWristX,rightWristY,20);
+        songplaying="stop"
+    
+    if(Song1="false")
+    {
+        song1=loadSound(song1)
+        document.getElementById("song_name").innerHTML="Harry Potter Theme song";
+     }
+}
+}
+
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.X;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX="+ leftWristX +"leftWristY = "+ leftWristY);
+        righttWristX=results[0].pose.rightWrist.X;
+       rightWristY=results[0].pose.rightWrist.y;
+       console.log("rightWristX="+ rightWristX +"rightWristY = "+ rightWristY);
+
+    }
 }
